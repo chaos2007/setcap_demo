d815d5a85226bb2874cfb41d8277fddf134702f3
Don Pollitz <chaos2007@gmail.com>
2018-02-07 21:30:21 -0600

    Change order of example 3.

--- 03_security_implication_of_running_as_root/dont_run_root.py
+++ 03_security_implication_of_running_as_root/dont_run_root.py
@@ -2,9 +2,8 @@ from scapy.all import *
 import os
 import shutil
 
-os.remove("/tmp/dont_delete")
-#shutil.rmtree("/")
-
 packet = Ether()/Dot1Q()/IP()
 send(packet)
 
+os.remove("/tmp/dont_delete")
+#shutil.rmtree("/")
